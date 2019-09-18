7917438e763df72fbca6714e6c6a7c97f9ba4040
Suhas Bansude <suhasb@mkclindia.local>
2019-09-18 11:05:51 +0530

    'namechaneg'

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <div>
-         Hi Suhas
+         Hi Suhas how are you
         </div>
     </div>
   );
